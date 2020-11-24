26ce03fc9e76e7e2f98acc7f45754399b9ebc9db
Liang Xu <xl0418@gmail.com>
2020-11-24 10:20:47 +0100

    The birth and speciation events are implemented. Implementing extinction is the next.

--- R/PlantCommunitySim.R
+++ R/PlantCommunitySim.R
@@ -19,6 +19,7 @@ PlantCommunitySim <-
            sig_disp,
            tend,
            output,
+           speciation_rate,
            num_cores = 4,
            torus = TRUE) {
     numCores <- min(num_cores, parallel::detectCores())
@@ -29,27 +30,53 @@ PlantCommunitySim <-
     ini.col <- sample(seq(0, L), size = 1)
     community[ini.row, ini.col] <- 1
     abundance_vector <- c(L ^ 2 - 1, 1)
+    coordinate_bank <-
+      cbind(rep(c(1:L), L), rep(c(1:L), each = L))
 
     for (t in c(1:tend)) {
+      # sample the dead individual
       dead.row <- sample(seq(0, L), size = 1)
       dead.col <- sample(seq(0, L), size = 1)
       dead.species <- community[dead.row, dead.col]
+      # reduce the corresponding abundance
       abundance_vector[dead.species + 1] <-
         abundance_vector[dead.species + 1] - 1
-      coordinate_bank <-
-        cbind(rep(c(1:L), each = L), rep(c(1:L), L))
+      # sample the birth or speciation event
+      sample_birth_speciation_event <-
+        sample(c(1, 2),
+               size = 1,
+               prob = c(1 - speciation_rate, speciation_rate))
+      # the birth event
+      if (sample_birth_speciation_event == 1) {
+        # calculate the colonization probability of individuals
+        colonization_probability_cluster <-
+          parallel::mclapply(
+            1:L ^ 2,
+            PlantCommunity::colonization_probability,
+            coordinate_bank = coordinate_bank,
+            coordinate2 = c(dead.row, dead.col),
+            jc_strength = jc_strength,
+            abundance_vector = abundance_vector,
+            L = L,
+            sig_disp = sig_disp,
+            community = community
+          )
+        colonization_probability_vector <-
+          unlist(colonization_probability_cluster)
+        # sample which individual gives the birth to the vacant site
+        sample.parent.to.give.birth <-
+          sample(community[1:L ^ 2], size = 1, prob = colonization_probability_vector)
+        community[dead.row, dead.col] <- sample.parent.to.give.birth
+        # update the corresponding abundance
+        abundance_vector[sample.parent.to.give.birth + 1] <-
+          abundance_vector[sample.parent.to.give.birth + 1] + 1
 
-      colonization_probability_cluster <-
-        parallel::mclapply(
-          1:L ^ 2,
-          colonization_probability,
-          coordinate_bank = coordinate_bank,
-          coordinate2 = c(dead.row, dead.col),
-          jc_strength = jc_strength,
-          abundance_vector = abundance_vector,
-          L = L,
-          sig_disp = sig_disp,
-          community = community
-        )
+      } else{
+        community[dead.row, dead.col] <- length(abundance_vector)
+        abundance_vector <- c(abundance_vector, 1)
+      }
+
+
+
     }
   }
--- tests/testthat/test-colonization_probability.R
+++ tests/testthat/test-colonization_probability.R
@@ -1,3 +1,13 @@
 test_that("self_excluded", {
-  expect_equal(2 * 2, 4)
+  expect_identical(colonization_probability(bank_index = 1,
+                                            coordinate_bank = t(as.matrix(c(1,1))),
+                                            coordinate2 = c(1, 1),
+                                            jc_strength = 0.5,
+                                            abundance_vector = c(100),
+                                            L = 10,
+                                            sig_disp = 1,
+                                            community = matrix(0, 10, 10),
+                                            torus = TRUE), 0)
 })
+
+
